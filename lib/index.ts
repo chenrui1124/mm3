3ba1bb67c48daa5f196b2484794dcbf90f8a9dcb
Rui <75087533+chenrui1124@users.noreply.github.com>
2023-12-26 07:45:20 +0800

    + type Either

--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,3 @@ export type * from './ts'
 
 export * from './atomic_css'
 export * from './common'
-export * from './vue'
--- lib/ts/index.ts
+++ lib/ts/index.ts
@@ -1,2 +1,3 @@
 export type { CSSInJS } from './css_in_js'
+export type { Either } from './either'
 export type { PureCSSIconClass } from './pure_css_icon_class'
