e1c3a23fed660a7452d9a580ad33cf98f361935d
Rui <75087533+chenrui1124@users.noreply.github.com>
2024-01-05 20:39:41 +0800

    Write test

--- test/common/has_prop.test.ts
+++ test/common/has_prop.test.ts
@@ -1,9 +1,14 @@
-import { test, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { hasProp } from '@lib'
 
-test('hasProp', () => {
-    expect(hasProp({ a: 1 }, 'a')).toBe(true)
-    expect(hasProp({ a: 1 }, 'b')).toBe(false)
-    expect(hasProp({ 1: 'a' }, 1)).toBe(true)
-    expect(hasProp({ '1': 'a' }, 1)).toBe(true)
+describe('hasProps', () => {
+    it('is true', () => {
+        expect(hasProp({ a: 1 }, 'a')).toBe(true)
+        expect(hasProp({ 1: 'a' }, 1)).toBe(true)
+        expect(hasProp({ '1': 'a' }, 1)).toBe(true)
+    })
+
+    it('is false', () => {
+        expect(hasProp({ a: 1 }, 'b')).toBe(false)
+    })
 })
--- test/common/lorem.test.ts
+++ test/common/lorem.test.ts
@@ -1,12 +1,30 @@
-import { test, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { lorem } from '@lib'
 
-test('lorem', () => {
-    expect(lorem()).toBe(
-        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat nulla ad magnam labore maxime architecto neque beatae necessitatibus, quae atque. Aliquam quaerat incidunt labore ipsa aliquid temporibus delectus architecto totam?'
-    )
-    expect(lorem('sc')).toContain('，')
-    expect(lorem('sc')).toContain('。')
-    expect(lorem('sc')).not.toContain(',')
-    expect(lorem('sc')).not.toContain('.')
+describe('lorem', () => {
+    it('return English text', () => {
+        expect(lorem()).toBe(
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat nulla ad magnam labore maxime architecto neque beatae necessitatibus, quae atque. Aliquam quaerat incidunt labore ipsa aliquid temporibus delectus architecto totam?'
+        )
+    })
+
+    it('return Chinese text', () => {
+        expect(lorem('sc')).toBe(
+            '鉴于对人类家庭所有成员的固有尊严及其平等的和不移的权利的承认，乃是世界自由、正义与和平的基础，鉴于对人权的无视和侮蔑已发展为野蛮暴行，这些暴行玷污了人类的良心，而一个人人享有言论和信仰自由并免予恐惧和匮乏的世界的来临，已被宣布为普通人民的最高愿望。'
+        )
+    })
+
+    it('only contain Chinese punctuation mark', () => {
+        expect(lorem('sc')).toContain('，')
+        expect(lorem('sc')).toContain('。')
+        expect(lorem('sc')).not.toContain(',')
+        expect(lorem('sc')).not.toContain('.')
+    })
+
+    it('only contain English punctuation mark', () => {
+        expect(lorem()).toContain(',')
+        expect(lorem()).toContain('.')
+        expect(lorem()).not.toContain('，')
+        expect(lorem()).not.toContain('。')
+    })
 })
